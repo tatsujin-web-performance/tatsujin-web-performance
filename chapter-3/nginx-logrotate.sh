7e6a02e922763383a36bcc8bc1eed9a023fa6c13
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2022-05-09 11:38:36 +0900

    add nginx-logrotate.sh

---
+++ chapter-3/nginx-logrotate.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# 実行時点の日時を YYYYMMDD-HHMMSS 形式で付与したファイル名にローテートする
+mv /var/log/nginx/access.log /var/log/nginx/access.log.`date +%Y%m%d-%H%M%S`
+
+# nginxにログファイルを き直すシグナルを送信する
+nginx -s reopen
