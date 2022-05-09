894fb5488933cc47fad686c6c551fdf1277b935a
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2022-05-09 11:39:33 +0900

    fix typo

--- chapter-3/nginx-logrotate.sh
+++ chapter-3/nginx-logrotate.sh
@@ -3,5 +3,5 @@
 # 実行時点の日時を YYYYMMDD-HHMMSS 形式で付与したファイル名にローテートする
 mv /var/log/nginx/access.log /var/log/nginx/access.log.`date +%Y%m%d-%H%M%S`
 
-# nginxにログファイルを き直すシグナルを送信する
+# nginxにログファイルを開き直すシグナルを送信する
 nginx -s reopen
